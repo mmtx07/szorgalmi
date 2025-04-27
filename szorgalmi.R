377950d2ca3072f72366ea243a7c2abea0cd613f
mmtx07 <magyarmate07@gmail.com>
2025-04-27 16:04:39 +0200

    Add 2 more categories and values

--- szorgalmi.R
+++ szorgalmi.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 
 
-data <- data.frame(category = c("A", "B", "C", "D", "E"),
-                   value = c(23, 17, 35, 29, 12))
+data <- data.frame(category = c("A", "B", "C", "D", "E", "F", "G"),
+                   value = c(23, 17, 35, 29, 12, 7, 25))
 
 
 ggplot(data, aes(x = category, y = value, fill = category)) +
