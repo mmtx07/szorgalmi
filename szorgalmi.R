78152dbb53225f1dcef41548275294da980cadee
Slim Mekki <mekkislimy@gmail.com>
2025-04-27 21:06:48 +0200

    Slim elvégezte a módosításokat: low_value_data, új ggplot és átlagfeletti kategek száma.

--- szorgalmi.R
+++ szorgalmi.R
@@ -11,3 +11,17 @@ ggplot(data, aes(x = category, y = value, fill = category)) +
 
 mean_value <- mean(data$value)
 median_value <- median(data$value)
+
+
+#Slim módosításai
+low_value_data <- subset(data, value < mean_value)
+print(low_value_data)
+
+
+ggplot(data, aes(x = category, y = value)) +
+  geom_line(group = 1) +
+  labs(title = "Line Chart of Category Values", x = "Category", y = "Value")
+
+
+above_mean <- sum(data$value > mean_value)
+print(paste("Az átlag felett lévő kategóriák száma:", above_mean))
